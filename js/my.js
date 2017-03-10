e53a11c84e6fdb20b3826dacfec465204962c5c4
stemye <stephen.myers1@gmail.com>
2017-03-09 19:12:36 -0500

    Added more content and jquery transitions to body text

--- js/my.js
+++ js/my.js
@@ -13,3 +13,31 @@ $(window).resize(function() {
  }
 
 });
+
+$(function(){
+    $("#myHeader").load("header.html");
+});
+
+//Hide all my content except the home-welcome text
+$(".about").hide();
+$(".wiki").hide();
+$(".download").hide();
+$(".contact").hide();
+
+$("#home").click(function(){
+    $(".home").show("slow");
+    $(".about").hide("slow");
+    $(".wiki").hide("slow");
+});
+
+$("#about").click(function(){
+    $(".about").show("slow");
+    $(".home").hide("slow");
+    $(".wiki").hide("slow");
+});
+
+$("#wiki").click(function(){
+    $(".wiki").show("slow");
+    $(".home").hide("slow");
+    $(".about").hide("slow");
+});
